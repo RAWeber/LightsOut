3b108c232c97f7fba6048ec6f17beb1cdc43e7e6
Jon T Scott <jonscott20@gmail.com>
2016-11-19 05:21:35 -0800

    Added the score keeping functionality based of the system clock. Displays as a running total in the game scene and a static value in GameOver

--- core/src/com/wolfpack/game/screens/GameOverScreen.java
+++ core/src/com/wolfpack/game/screens/GameOverScreen.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.math.Vector2;
 import com.wolfpack.game.GameApp;
 import com.wolfpack.game.utilities.Animator;
@@ -14,10 +16,16 @@ public class GameOverScreen implements Screen {
   private final GameApp game;
   private OrthographicCamera camera;
   
-  private int score;
+  
+  private float scoreWidth;
+  private float scoreHeight;
+  private String scoreText;
+  
   
   private Animator title;
   private Vector2 titlePosition;
+  
+  private BitmapFont font;
   
   public GameOverScreen(GameApp game, int score) {
     this.game = game;
@@ -25,8 +33,10 @@ public class GameOverScreen implements Screen {
     camera = new OrthographicCamera();
     camera.setToOrtho(false, 1280, 720);
     titlePosition = new Vector2(camera.viewportWidth / 2 - 320, camera.viewportHeight/2 - 320);
-    title = new Animator("GameOver.png", titlePosition, 1, 2, 2);
-    this.score = score;
+    title = new Animator("GameOver.png", titlePosition, 1, 2, 1);
+    scoreText = "Score: " + score;
+    font = new BitmapFont();
+    font.getData().setScale(1.5f);
   }
   
   @Override
@@ -37,6 +47,11 @@ public class GameOverScreen implements Screen {
 
   @Override
   public void render(float delta) {
+	  
+	GlyphLayout layout = new GlyphLayout();
+	layout.setText(font, scoreText);
+	scoreWidth = layout.width;
+	scoreHeight = layout.height;
     
     Gdx.gl.glClearColor(0, 0, 0.2f, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -47,6 +62,8 @@ public class GameOverScreen implements Screen {
     GameApp.getSpritebatch().begin();
     //Render in here
     title.render();
+    font.draw(GameApp.getSpritebatch(), scoreText, camera.viewportWidth / 2 - scoreWidth / 2,
+   		 scoreHeight / 2 + 20);
     GameApp.getSpritebatch().end();
     
     // Enter can be used to restart the game
